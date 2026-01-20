578d765e074e6f28011809b56c28d66672a562f6
Sir Mino <115249835+SirVoly@users.noreply.github.com>
2026-01-20 11:47:23 +0000

    Improved the agg command so it saves the items as posts

--- internal/cli/handler_agg.go
+++ internal/cli/handler_agg.go
@@ -2,11 +2,14 @@ package cli
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"time"
 
 	"github.com/SirVoly/gator/internal/database"
 	"github.com/SirVoly/gator/internal/rssfeed"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -59,6 +62,80 @@ func scrapeFeeds(s *state) {
 	)
 
 	for _, item := range(rf.Channel.Item) {
-		fmt.Println(item.Title)
+		err = addPost(s, item, feed.ID)
+		if err != nil {
+			// Unexpected error: Stop scraping
+			fmt.Println(err)
+			return
+		}
 	}
+}
+
+func addPost(s * state, item rssfeed.RSSItem, feed_id uuid.UUID) error {
+	id := uuid.New()
+	t := time.Now()
+	desc := sql.NullString{
+		String: item.Description,
+		Valid: (item.Description != ""),
+	}
+
+	pubDate := sql.NullTime{
+		Time: time.Time{},
+		Valid: item.PubDate != "",
+	}
+	if pubDate.Valid {
+		parsedTime, err := parseFlexibleTime(item.PubDate)
+		if err != nil {
+			return fmt.Errorf("could not parse %q: %w", s, err)
+		}
+		pubDate.Time = parsedTime
+	}
+
+	_, err := s.db.CreatePost(
+		context.Background(),
+		database.CreatePostParams{
+			ID: id,
+			CreatedAt: t,
+			UpdatedAt: t,
+			Title: item.Title,
+			Url: item.Link,
+			Description: desc,
+			PublishedAt: pubDate,
+			FeedID: feed_id,
+		},
+	)
+
+	if err != nil {
+		pg_err, ok := err.(*pq.Error)
+		if ok && pg_err.Code.Name() == "unique_violation" { //Error Code 23505
+			return nil
+		}
+		// Unexpected error
+		return err
+	}
+
+	// Post is successfully made
+	return nil
+}
+
+// Help functions
+
+var layouts = []string{
+	time.RFC3339,            // "2006-01-02T15:04:05Z07:00"
+	"2006-01-02 15:04:05",   // "2024-01-02 15:04:05"
+	"2006-01-02",            // "2024-01-02"
+	"02/01/2006 15:04",      // "02/01/2024 15:04"
+	time.RFC1123Z, // "Tue, 20 Jan 2026 11:15:33 +0000"
+}
+
+func parseFlexibleTime(s string) (time.Time, error) {
+	var lastErr error
+	for _, layout := range layouts {
+		t, err := time.Parse(layout, s)
+		if err == nil {
+			return t, nil
+		}
+		lastErr = err
+	}
+	return time.Time{}, fmt.Errorf("could not parse %q: %w", s, lastErr)
 }
